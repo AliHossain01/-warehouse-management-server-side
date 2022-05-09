e10bc0e974156504f87b1d047cb0c0e31a9c203f
AliHossain01 <alihossaincse01@gmail.com>
2022-05-09 22:20:14 +0600

    new api

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ async function run() {
             res.send(services);
         });
 
+        app.get('/hero', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
